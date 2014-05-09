5e82253a2283be285a92a228e78439645a85ee64
Hanif, Hakimuddin <haha@amazon.com>
2014-05-09 00:23:00 -0700

    Finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have content 'Sample app'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have content Help" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+  end
+
+  describe "About page" do
+
+    it "should have content 'About Us'" do
+      visit '/static_pages/aboutus'
+      expect(page).to have_content('About Us')
+    end
+
+  end
+
+end

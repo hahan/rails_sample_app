76d9ab66fcaba02ee6796c38e293fa91cea77642
Hanif, Hakimuddin <haha@amazon.com>
2014-05-13 23:33:00 -0700

    Done till 5.3.4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def aboutus
   end
+
+  def contact
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,35 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have content 'Sample app'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have title Home" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-
-      expect(page).to have_title("Home")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("Home") }
   end
 
   describe "Help page" do
+    before {visit help_path}
 
-    it "should have content Help" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    it { should have_content('Help') }
 
   end
 
   describe "About page" do
 
-    it "should have content 'About Us'" do
-      visit '/static_pages/aboutus'
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
+    it { should have_content('About Us') }
+  end
+
+  describe "Contact page" do
+
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title("Contact") }
   end
 
 end
